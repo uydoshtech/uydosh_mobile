950ffe303980a7cd1c138562f6b78ba5dfc965a7
artur-musin <arturchik.musin@gmail.com>
2026-03-28 21:41:19 -0700

    Android splash screen

--- android/app/src/main/kotlin/com/uydosh/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/uydosh/app/MainActivity.kt
@@ -1,5 +1,14 @@
 package com.uydosh.app
 
+import android.os.Build
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            splashScreen.setOnExitAnimationListener { view -> view.remove() }
+        }
+        super.onCreate(savedInstanceState)
+    }
+}
