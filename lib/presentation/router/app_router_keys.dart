926ea42b8f94490e044a07599f505c132cf5e95a
artur-musin <uydoshtech@gmail.com>
2026-04-14 03:17:32 -0700

    refactoring

---
+++ lib/presentation/router/app_router_keys.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+import "package:uy_dosh/presentation/widgets/tutorial/search_tutorial_overlay.dart";
+
+/// Global keys used as tutorial targets inside the app shell UI.
+final GlobalKey<TutorialTargetWrapperState> profileIconTutorialKey =
+    GlobalKey<TutorialTargetWrapperState>();
+
+final GlobalKey<TutorialTargetWrapperState> notificationsBellTutorialKey =
+    GlobalKey<TutorialTargetWrapperState>();
+
