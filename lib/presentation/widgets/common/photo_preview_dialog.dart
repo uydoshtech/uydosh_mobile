24b6a23945c6370bbf402ec2c91d455030c34afd
artur-musin <uydoshtech@gmail.com>
2026-05-01 03:20:10 -0700

    Google Console branding

--- lib/presentation/widgets/common/photo_preview_dialog.dart
+++ lib/presentation/widgets/common/photo_preview_dialog.dart
@@ -2,7 +2,6 @@ import "dart:io";
 
 import "package:cached_network_image/cached_network_image.dart";
 import "package:flutter/material.dart";
-import "package:uy_dosh/base/constants/app_colors.dart";
 import "package:uy_dosh/presentation/widgets/common/theme_icon.dart";
 import "package:uy_dosh/presentation/widgets/common/three_d_app_bar_icon_button.dart";
 
@@ -63,7 +62,7 @@ class PhotoPreviewDialog extends StatelessWidget {
             ClipRRect(
               borderRadius: BorderRadius.circular(16),
               child: ColoredBox(
-                color: BlueThemeColors.navigationBackground,
+                color: const Color(0xFF0B1220),
                 child: InteractiveViewer(
                   minScale: 1,
                   maxScale: 4,
