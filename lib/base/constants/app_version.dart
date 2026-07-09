5cf7661a67764290da6e415fa973e263e8b98723
artur-musin <uydoshtech@gmail.com>
2026-07-10 03:19:58 +0500

    Bump version to 1.1.258+383

--- lib/base/constants/app_version.dart
+++ lib/base/constants/app_version.dart
@@ -3,7 +3,7 @@
 
 class AppVersion {
   static const String version = "1.1.258";
-  static const String buildNumber = "382";
+  static const String buildNumber = "383";
   static const String fullVersion = "$version+$buildNumber";
   static const String displayVersion = "Version $version";
 }
