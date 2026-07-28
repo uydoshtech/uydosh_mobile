6d85b6a1463ea005a26bae4ba2a0e57f64b431ad
UyDosh <uydoshtech@gmail.com>
2026-07-29 03:07:08 +0500

    version bump

--- lib/base/constants/app_version.dart
+++ lib/base/constants/app_version.dart
@@ -3,7 +3,7 @@
 
 class AppVersion {
   static const String version = "1.1.258";
-  static const String buildNumber = "421";
+  static const String buildNumber = "422";
   static const String fullVersion = "$version+$buildNumber";
   static const String displayVersion = "Version $version";
 }
